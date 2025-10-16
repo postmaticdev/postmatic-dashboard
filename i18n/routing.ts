4c70a3f103f705c2d702f8cb931b30d05ec501f1
lanstheprodigy <alan.040219.gam@gmail.com>
2025-10-17 01:39:41 +0700

    fix (locale): fix locale for vercel deployment

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,4 +1,4 @@
-// src/i18n/routing.ts
+// i18n/routing.ts
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
@@ -6,5 +6,4 @@ export const routing = defineRouting({
   defaultLocale: "id",
 });
 
-// aman untuk type:
 export type Locale = (typeof routing.locales)[number];
