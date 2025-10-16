2b0ab01795624c98ad5aba7adc55849b7b54466d
lanstheprodigy <alan.040219.gam@gmail.com>
2025-10-17 01:18:36 +0700

    fix (locale): fix locale for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   output: "standalone",
